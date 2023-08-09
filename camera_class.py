4e6b06d685bb0008148b8fe41f8b69f2c047ddf6
Stefan Steinhauser <ssteinhauser@steinhauser.tirol>
2023-08-09 21:08:38 +0200

    Add unbuffered stream reader

--- camera_class.py
+++ camera_class.py
@@ -3,6 +3,7 @@ import sys
 import io
 import gc
 import time
+import queue
 from collections import deque
 from datetime import datetime
 from threading import Thread
@@ -23,6 +24,32 @@ handler.setFormatter(formatter)
 log = logging.getLogger(__name__)
 log.addHandler(handler)
 log.setLevel(LOGGING_LEVEL)
+
+class VideoCapture:
+  def __init__(self, name):
+    self.cap = cv.VideoCapture(name)
+    self.cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
+    self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
+    self.q = queue.Queue()
+    t = threading.Thread(target=self._reader)
+    t.daemon = True
+    t.start()
+
+  # read frames as soon as they are available, keeping only most recent one
+  def _reader(self):
+    while True:
+      ret, frame = self.cap.read()
+      if not ret:
+        break
+      if not self.q.empty():
+        try:
+          self.q.get_nowait()   # discard previous (unprocessed) frame
+        except queue.Empty:
+          pass
+      self.q.put(frame)
+
+  def read(self):
+    return self.q.get()
 
 raspicam = False
 raspicam2 = False
@@ -60,11 +87,9 @@ class Camera:
             i = 0
         elif not raspicam:
             streamURL = os.getenv('STREAM_URL')
-            cap = cv.VideoCapture(streamURL)
+            cap = VideoCapture(streamURL)
             # Set the resolution
             log.info("Setting up camera")
-            cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
-            cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
             i = 0
 
         while True:
