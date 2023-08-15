a83851f8db3fb3eede91c3e5b1e490126bba11d3
Stefan Steinhauser <ssteinhauser@steinhauser.tirol>
2023-08-15 19:05:55 +0200

    Fix index out of bounds

--- cascade.py
+++ cascade.py
@@ -237,7 +237,8 @@ class Sequential_Cascade_Feeder():
         #Feed the latest image in the Queue through the cascade
         if self.fps_offset != 0:
             # TODO just a quickfix - find better implementation
-            self.fps_offset = max(self.fps_offset, len(self.main_deque) - 1)
+            max_index = len(self.main_deque) - 1
+            self.fps_offset = min(max(self.fps_offset, max_index), max_index)
         cascade_obj = self.feed(target_img=self.main_deque[self.fps_offset][1], img_name=self.main_deque[self.fps_offset][0])[1]
         #log.info('Runtime:'+ str(time.time() - start_time))
         done_timestamp = datetime.now(tz=ZoneInfo('Europe/Zurich')).strftime("%Y_%m_%d_%H-%M-%S.%f")
